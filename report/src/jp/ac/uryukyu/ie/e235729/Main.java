5651d9bf817147553bfdfeae6b12c7a03cddefc5
Shimoji Yuto <e235729@ie.u-ryukyu.ac.jp>
2024-01-21 21:27:11 +0900

    2

---
+++ report/src/jp/ac/uryukyu/ie/e235729/Main.java
@@ -0,0 +1,15 @@
+package jp.ac.uryukyu.ie.e235729;
+public class Main {
+    
+    public static void main(String[] args) {
+        //ルールの説明文の用意
+        String rules = "３つの数字を当てる数当てゲームです\n" + "数字は０〜９まであり、重複しないものとします\n" + "Hit...数字と位置が当たり\n" + "Blow...数字だけ当たり(位置は違う)\n" ;
+        
+        //ルールの出力
+        System.out.println(rules);
+        //答えの用意
+        Answer answer = new Answer();
+        System.out.println("answer");
+
+    }
+}
