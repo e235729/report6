01b9971aab57cdfbfd06e5961bd80b81047b977b
Shimoji Yuto <e235729@ie.u-ryukyu.ac.jp>
2024-01-22 15:19:05 +0900

    説明追加

--- report/app/src/main/java/jp/ac/uryukyu/ie/e235729/AnswerGenerator.java
+++ report/app/src/main/java/jp/ac/uryukyu/ie/e235729/AnswerGenerator.java
@@ -1,7 +1,14 @@
 package jp.ac.uryukyu.ie.e235729;
 import java.util.Random;
-
+/**
+ * 正解を生成するクラスです。
+ */
 class AnswerGenerator {
+        /**
+     * 1から9までの異なる数字からなる正解を生成します。
+     *
+     * @return 生成された正解の数字配列
+     */
     public int[] generateAnswer() {
         int num1, num2, num3;
         Random rnd = new Random();
